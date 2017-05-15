10c761271fe7cc51e60446681a3a950898d3148c
viacheslav seledkin <vseledkin@ws24364.avicomp.com>
2017-05-15 20:22:16 +0300

    add split utility

--- gorpora.go
+++ gorpora.go
@@ -1,1 +1,63 @@
 package gorpora
+
+import (
+	"bufio"
+	"fmt"
+	"html"
+	"os"
+	"strings"
+	"unicode"
+)
+
+func NormalizeHtmlEntities() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+		os.Stdout.WriteString(html.UnescapeString(line))
+	}
+}
+
+func Split() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+		os.Stdout.WriteString(split2Tokens(line))
+		os.Stdout.WriteString("\n")
+	}
+}
+
+func split2Tokens(s string) string {
+	token := ""
+	var split []string
+	for _, r := range s {
+		switch {
+		case unicode.IsPunct(r) || unicode.IsSymbol(r):
+			if len(token) > 0 {
+				split = append(split, token)
+				token = ""
+			}
+			split = append(split, string(r))
+		case len(token) == 0 && unicode.IsSpace(r):
+			continue // skip leading space
+		case len(token) == 0 && !unicode.IsSpace(r):
+			token = string(r)
+		case len(token) > 0 && !unicode.IsSpace(r):
+			token += string(r)
+		case len(token) > 0 && unicode.IsSpace(r):
+			split = append(split, token)
+			token = ""
+		default:
+			panic(fmt.Errorf("unknown symbol %q", r))
+		}
+	}
+	if len(token) > 0 {
+		split = append(split, token)
+	}
+	return strings.Join(split, " ")
+}
--- gorpora/gorpora.go
+++ gorpora/gorpora.go
@@ -10,33 +10,57 @@ import (
 )
 
 const (
-	normalizeHtmlEntities = "normalize.html.etities"
+	normalizeHtmlEntities = "normalize.html.entities"
+	split                 = "split"
 )
+
+var MAX int
 
 func main() {
 	normalizeHtmlEntitiesCommand := flag.NewFlagSet(normalizeHtmlEntities, flag.ExitOnError)
+	normalizeHtmlEntitiesCommand.IntVar(&MAX, "max", 0, "maximum number of lines to process")
+
+	splitCommand := flag.NewFlagSet(split, flag.ExitOnError)
+	splitCommand.IntVar(&MAX, "max", 0, "maximum number of lines to process")
+
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
 		fmt.Fprintf(os.Stderr, "gorpora <command> arguments\n")
 		fmt.Fprintf(os.Stderr, "commands are:\n")
 		fmt.Fprintf(os.Stderr, "%s\n", normalizeHtmlEntities)
 		normalizeHtmlEntitiesCommand.PrintDefaults()
+		fmt.Fprintf(os.Stderr, "%s\n", split)
+		splitCommand.PrintDefaults()
 		flag.PrintDefaults()
 	}
 	flag.Parse()
 	log.SetOutput(os.Stderr)
 
-	switch os.Args[1] {
-	case normalizeHtmlEntities:
-		normalizeHtmlEntitiesCommand.Parse(os.Args[:0])
-	default:
-		log.Printf("%q is not valid command.\n", os.Args[1])
+	if len(os.Args) == 1 {
+		flag.Usage()
 		os.Exit(1)
 	}
 
-	// BUILD COMMAND ISSUED
+	switch os.Args[1] {
+	case normalizeHtmlEntities:
+		normalizeHtmlEntitiesCommand.Parse(os.Args[2:])
+	case split:
+		splitCommand.Parse(os.Args[2:])
+	default:
+		log.Printf("%q is not valid command.\n", os.Args[1])
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	// NORMALIZE ENTITIES COMMAND ISSUED
 	if normalizeHtmlEntitiesCommand.Parsed() {
 		gorpora.NormalizeHtmlEntities()
+		return
+	}
+
+	// SPLIT COMMAND ISSUED
+	if splitCommand.Parsed() {
+		gorpora.Split()
 		return
 	}
 }
