85f3e696f81b3cf1c49663e347403b8bd07b11ed
vs <vseledkin@gmail.com>
2017-05-23 08:22:10 +0300

    Shallow unique

--- gorpora.go
+++ gorpora.go
@@ -45,9 +45,9 @@ func GetMD5Hash(bytes []byte) string {
 
 func Unique(DEBUG bool) {
 	reader := bufio.NewReader(os.Stdin)
-	dic := make(map[string]interface{})
+	dic := make(map[string]int)
 	lineCount := 0
-	var empty interface{}
+	uniqueCount := 0
 	for {
 		line, err := reader.ReadString('\n')
 		if err != nil {
@@ -59,20 +59,31 @@ func Unique(DEBUG bool) {
 		if _, ok := dic[hash]; ok {
 			if DEBUG {
 				os.Stdout.WriteString("DUBLICATE: " + line)
-				//os.Stdout.Write([]byte{'\n'})
+				dic[hash] += 1
 			}
 		} else {
-			dic[hash] = empty
+			dic[hash] = 1
 			if !DEBUG {
 				os.Stdout.WriteString(line)
-				//os.Stdout.Write([]byte{'\n'})
+				uniqueCount++
 			}
+		}
+		if lineCount%10e6 == 0 {
+			log.Printf("clean: dic size %d %d total", len(dic), lineCount)
+			for k, v := range dic {
+				if v < 2 {
+					delete(dic, k)
+				} else {
+					dic[k]--
+				}
+			}
+			log.Printf("dic size %d %d total", len(dic), lineCount)
 		}
 	}
 
 	log.Println(lineCount, "lines total")
-	log.Println(len(dic), "unique lines")
-	log.Println(lineCount-len(dic), "non unique lines")
+	log.Println(uniqueCount, "unique lines")
+	log.Println(lineCount-uniqueCount, "non unique lines")
 }
 
 func FilterLanguage(languages []string) {
